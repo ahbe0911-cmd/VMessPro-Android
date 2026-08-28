313b11a096c6898cc6b8739ea2202f8c24f5c666
ahbe0911-cmd <ahbe0911@gmail.com>
2026-08-28 15:52:57 +0330

    Bump polished build to 0.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.vmesspro.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.5.0"
+        versionCode = 6
+        versionName = "0.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
