f35f60569c0c48a5e041c7f05394ef753fec40a2
ahbe0911-cmd <ahbe0911@gmail.com>
2026-08-28 05:15:33 +0330

    add VPN telemetry broadcast contract

--- app/src/main/java/com/vmesspro/android/core/CoreContract.kt
+++ app/src/main/java/com/vmesspro/android/core/CoreContract.kt
@@ -4,6 +4,7 @@ internal object CoreContract {
     const val ACTION_CONNECT = "com.vmesspro.android.core.CONNECT"
     const val ACTION_DISCONNECT = "com.vmesspro.android.core.DISCONNECT"
     const val ACTION_STATE = "com.vmesspro.android.core.STATE"
+    const val ACTION_TELEMETRY = "com.vmesspro.android.core.TELEMETRY"
 
     const val EXTRA_PROFILE_ID = "profile_id"
     const val EXTRA_SPLIT_MODE = "split_mode"
@@ -14,6 +15,14 @@ internal object CoreContract {
     const val EXTRA_STATE = "state"
     const val EXTRA_REASON = "reason"
     const val EXTRA_SINCE = "since"
+
+    const val EXTRA_UPLINK = "uplink"
+    const val EXTRA_DOWNLINK = "downlink"
+    const val EXTRA_UPLINK_TOTAL = "uplink_total"
+    const val EXTRA_DOWNLINK_TOTAL = "downlink_total"
+    const val EXTRA_CONNECTIONS_IN = "connections_in"
+    const val EXTRA_CONNECTIONS_OUT = "connections_out"
+    const val EXTRA_TRAFFIC_AVAILABLE = "traffic_available"
 
     const val STATE_DISCONNECTED = "DISCONNECTED"
     const val STATE_PREPARING = "PREPARING"
